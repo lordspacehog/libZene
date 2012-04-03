db1739f946c7f193b32ae37f3cf888527a8ec69b
Loki <HHSSpacehog@gmail.com>
2012-04-03 12:33:18 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using DirectoryListing;
+
+namespace DirectoryListing {
+    class Program {
+        static void Main(string[] args) {
+            //who the fuck knows what this shit is doing...
+            List<string> lstProcessData = new List<string>();
+            int nThreadCount=4;
+            ManualResetEvent[] doneEvents;
+            string dir = @"c:\music\";
+
+            getFileListing(dir, ref lstProcessData);
+
+            doneEvents = new ManualResetEvent[lstProcessData.Count()];
+            int i = 0;
+            foreach (string strDir in lstProcessData) {
+                doneEvents[i] = new ManualResetEvent(false);
+                WorkItem wkDir = new WorkItem(strDir, doneEvents[i]);
+                ThreadPool.SetMaxThreads(nThreadCount,nThreadCount);
+                ThreadPool.QueueUserWorkItem(wkDir.ThreadPoolCallback, i);
+                i++;
+            }
+
+            WaitHandle.WaitAll(doneEvents);
+            Console.WriteLine("All files Processed");
+        }
+
+        public static void getFileListing(string baseDir,ref List<string> lstDir) {
+
+            if (Directory.GetDirectories(baseDir).Count() != 0) {
+                foreach (string dirName in Directory.GetDirectories(baseDir)) {
+                    getFileListing(dirName, ref lstDir);
+                    lstDir.Add(dirName);
+                }
+            }
+            else {
+                lstDir.Add(baseDir);
+            }
+                
+        }
+    }
+}
---
+++ WorkItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace DirectoryListing {
+    class WorkItem {
+        //Private variables
+        private string _strWork;
+        private List<string> _lstFiles;
+        private ManualResetEvent _doneEvent;
+
+        //Public interfaces
+        public string StrWork { get { return _strWork; } }
+        public List<string> FileList { get { return _lstFiles; } }
+
+        public WorkItem(string strWork, ManualResetEvent doneEvent) {
+            _strWork = strWork;
+            _doneEvent = doneEvent;
+
+            //initialize File list
+            _lstFiles = new List<string>();
+        }
+
+        public void ThreadPoolCallback(Object threadContext) {
+            int threadIndex = (int)threadContext;
+            Console.WriteLine("thread {0} started...", threadIndex);
+            
+            _lstFiles = ListFiles(_strWork);
+
+            if (_lstFiles != null) {
+                foreach (string strFile in _lstFiles) {
+                    Console.WriteLine(strFile);
+                }
+            }
+            else {
+                Console.WriteLine("no files in dir");
+            }
+            Console.WriteLine("thread {0} finished...", threadIndex);
+            _doneEvent.Set();
+        }
+
+        public List<string> ListFiles(string strProcessDir) {
+            List<string> lstWorking = new List<string>();
+
+            if (Directory.GetFiles(strProcessDir, "*.*").Count() != 0) {
+
+                foreach (string strFileName in Directory.GetFiles(strProcessDir, "*.*")) {
+                    lstWorking.Add(strFileName);
+                }
+                return lstWorking;
+            }
+            else {
+                return null;
+            }
+        }
+    }
+}
